536ad3b80bcd12e42ebbec189705b51b9e30d8b2
Shireesh (VirtualHostWin10) <shireeshj@gmail.com>
2020-06-20 00:25:17 +0530

    incremental commit

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
 
 namespace LiveDeviceDector2
 {
@@ -18,27 +19,34 @@ namespace LiveDeviceDector2
         private const int DBT_DEVICEREMOVECOMPLETE = 0X8004;
         private const int DBT_CONFIGCHANGED = 0X0018;
         private const int DBT_DEVICEQUERYREMOVEFAILED = 0X8002;
+        int displayDeviceCount = 0;
         protected override void WndProc(ref Message m)
         {
+            
             base.WndProc(ref m);
  
             if (m.Msg == DeviceNotification.WmDevicechange)
             {
-                Console.WriteLine(m.Msg);
-                Console.WriteLine(DeviceNotification.WmDevicechange);
-
+                displayDeviceCount = GetDisplayDeviceCount();
                 switch ((int)m.WParam)
                 {
-                    case DeviceNotification.DbtDeviceRemoveComplete:
-                        messageListBox.Items.Add("Device Removed"); // this is where you do your magic
-                        break;
+                    case DeviceNotification.DbtDeviceRemoveComplete: 
+                            messageListBox.Items.Add("Removed. Count:  " + displayDeviceCount); // this is where you do your magic
+                            break;
                     case DeviceNotification.DbtDeviceArrival:
-                        messageListBox.Items.Add("Device added"); // this is where you do your magic
+                        messageListBox.Items.Add("Added: Count: " + +displayDeviceCount); // this is where you do your magic
                         break;
                 }
             }
         }
 
+        private static int GetDisplayDeviceCount()
+        {
+            ManagementObjectCollection collection;
+            using (var finddevice = new ManagementObjectSearcher(@"select * from Win32_PnPEntity where service='monitor'"))
+                collection = finddevice.Get();
+            return collection.Count;
+        }
         public Form1()
         {
             InitializeComponent();
